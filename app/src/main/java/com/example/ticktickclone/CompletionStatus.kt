5559a7b422ec471f65e4e415fbd846ad57c7e44a
madalv <vlada.magal@isa.utm.md>
2023-06-02 19:27:52 +0300

    added recycler view

---
+++ app/src/main/java/com/example/ticktickclone/CompletionStatus.kt
@@ -0,0 +1,5 @@
+package com.example.ticktickclone
+
+enum class CompletionStatus {
+    DONE, WONT_DO, NOT_MARKED
+}
--- app/src/main/java/com/example/ticktickclone/TaskListFragment.kt
+++ app/src/main/java/com/example/ticktickclone/TaskListFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ticktickclone.databinding.TaskListFragmentBinding
 
 class TaskListFragment: Fragment() {
 
     private lateinit var binding: TaskListFragmentBinding
+    lateinit var tasks: ArrayList<Task>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +19,14 @@ class TaskListFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = TaskListFragmentBinding.inflate(layoutInflater, container, false)
+
+        val rvTasks = binding.taskListRv
+        tasks = Task.createTasks()
+        val adapter = TaskAdapter(tasks)
+
+        rvTasks.adapter = adapter
+        rvTasks.layoutManager = LinearLayoutManager(this.context)
+
         return binding.root
     }
 
